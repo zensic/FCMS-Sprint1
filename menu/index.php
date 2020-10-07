6075eaa956cfa0393b407b4ee80f7f53c5e94f8d
bong98 <55526360+bong98@users.noreply.github.com>
2020-10-07 22:41:26 +0800

    add menu.php

---
+++ menu/index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>Homepage</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand" href="#page-top"><img src='picture/logo.png' width='50' id="logo"></a>
+      <a class="navbar-brand js-scroll-trigger" href="#page-top" id="company">FoodEdge</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#page-top">Home</a></li>
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#menu">Menu</a></li>
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">About Us</a></li>
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#">Log In</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <section id="slideshow">
+      <div class="container">
+          <div class="col-md-12">
+              <h1>Slideshow</h1>
+          </div>
+      </div>
+</section>
+
+<section id="menu">
+  <div class="container">
+          <div class="row">
+              <div class="col-md-12 text-center">
+                  <h2>Menu Catering</h2>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-4">
+                  <div class="outer">
+                      <a href="menu.php">
+                          <div class="upper">
+                              <img src="picture/chinese.jpg" class="img-rounded" alt="chinese food">
+                          </div>
+                          <div class="lower">
+                              <span>Catering Package A</span>
+                          </div>
+                      </a>
+                  </div>
+              </div>
+              <div class="col-4">
+                  <div class="outer">
+                      <a href="menu.php">
+                          <div class="upper">
+                              <img src="picture/western.jpg" alt="western food">
+                          </div>
+                          <div class="lower">
+                              <span>Catering Package B</span>
+                          </div>
+                      </a>
+                  </div>
+              </div>
+              <div class="col-4">
+                  <div class="outer">
+                      <a href="menu.php">
+                          <div class="upper">
+                              <img src="picture/mix.jpg" alt="mix package">
+                          </div>
+                          <div class="lower">
+                              <span>Catering Package C</span>
+                          </div>
+                      </a>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+
+  <section id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Contact us</h2>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio fugiat voluptatem dolor, provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et repudiandae ipsa exercitationem, in, quo totam.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="py-5">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
---
+++ menu/menu.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>Catering Package Details</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand" href="index.php"><img src='picture/logo.png' width='50' id="logo"></a>
+      <a class="navbar-brand js-scroll-trigger" href="index.php" id="company">FoodEdge</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php">Home</a></li>
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="menu.php">Menu</a></li>
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#">About Us</a></li>
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#">Log In</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <section id="detail">
+      <div class="container">
+          <div class="row justify-content-center">
+              <div class="col-lg-4 col-md-6 col-sm-6 col-5" id="selected-image">
+                  <img src="picture/chinese.jpg" width="250" alt="western food" id="selected">
+              </div>
+              <div class="col-lg-4 col-md-6 col-sm-6 col-5" id="selected-detail">
+                  <p>Package A</p>
+                  <p>RM 500</p>
+                  <p>Menu Detail</p>
+                  <ul class="infomation">
+                    <li>Chinese Fried Rice</li>
+                    <li>Breaised Noodles</li>
+                    <li>Seafood with Chilli Crab Sauce</li>
+                    <li>Omelette</li>
+                    <li>Mixed Fresh Fruits</li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+</section>
+
+<section id="other">
+  <div class="container">
+          <div class="row">
+              <div class="col-md-12 text-center">
+                  <h2>Others</h2>
+              </div>
+          </div>
+          <div class="row  justify-content-center">
+              <div class="col-lg-3 col-md-3 col-sm-5 col-5">
+                  <div class="outer">
+                      <a href="#">
+                          <div class="upper">
+                              <img src="picture/western.jpg" alt="western food">
+                          </div>
+                          <div class="lower">
+                              <span>Catering Package B</span>
+                          </div>
+                      </a>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-3 col-sm-5 col-5">
+                  <div class="outer">
+                      <a href="#">
+                          <div class="upper">
+                              <img src="picture/mix.jpg" alt="mix package">
+                          </div>
+                          <div class="lower">
+                              <span>Catering Package C</span>
+                          </div>
+                      </a>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="py-5">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
