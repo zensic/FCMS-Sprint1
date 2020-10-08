54d8e28f1bf5828c859b999aa4dff2c3b4dc460a
simonlu97 <62094893+simonlu97@users.noreply.github.com>
2020-10-08 16:53:37 +0800

    Update user_creation_page_OT.php

--- user_creation_page_OT.php
+++ user_creation_page_OT.php
@@ -7,12 +7,45 @@
 		<meta charset="utf-8">
 		<meta name = "viewport" content = "width=device-width, initial-scales">
 		
-		<link rel="stylesheet" type="text/css" href="../css/global.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 		
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+		
+		<style>
+			*
+			{
+				color: #5E503F;
+				font-weight: bold;
+			}
+			h1
+			{
+				text-align: center;
+			}
+
+			body
+			{
+				background-image: url("images/background.jpg");
+				background-repeat: no-repeat;
+				background-position: center;
+				background-size: cover;
+				background-attachment: fixed;
+				
+			}
+			.form-container
+			{
+				border: 0px solid #fff; 
+				padding: 30px 30px; 
+				margin-top: 10vh;
+				box-shadow: -1px 4px 26px 11px rgba(0,0,0,0.50);
+				background-color:white;
+			}
+
+			.table-sortable tbody tr {
+				cursor: move;
+			}
+		</style>
 	</head>
 	<body>
 		<div class = "container-fluid">
