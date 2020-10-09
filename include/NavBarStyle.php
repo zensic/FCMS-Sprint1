9cacaed354088f81d0233c5fa372fbf13ff8a81a
Anthony <anthonyleehj@gmail.com>
2020-10-09 09:51:21 +0800

    Fixed navbar linking

--- include/NavBarStyle.php
+++ include/NavBarStyle.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   
     <!-- Custom styles for this template -->
-    <link href="styles/client_homepage.css" rel="stylesheet">
+    <link href="styles/client_catering.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
   
     </head>
