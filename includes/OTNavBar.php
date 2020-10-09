5f0507c8d1974fbb3f0ed2d7d359b9c5988ef098
Anthony <anthonyleehj@gmail.com>
2020-10-09 08:49:23 +0800

    Added navigation bar

---
+++ includes/OTNavBar.php
@@ -0,0 +1,36 @@
+<?php
+    echo '
+    <head>
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+  
+    <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
+  
+  </head>
+  
+  
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+  <div class="container">
+      <a class="navbar-brand" href="index.php"><img src="picture/logo.png" width="50" id="logo"></a>
+      <a class="navbar-brand js-scroll-trigger" href="index.php" id="company">FoodEdge</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php">Home</a></li>
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="menu.php">Menu</a></li>
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#">About Us</a></li>
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#">Log In</a></li>
+      </ul>
+      </div>
+  </div>
+  </nav>
+    '
+?>
+
