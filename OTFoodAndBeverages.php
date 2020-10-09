e5841e106b7ce55a913ad7b85f53b9f561647871
zensic <anthonyleehj@gmail.com>
2020-10-09 15:56:04 +0800

    adjusted foodnbeverages margins

--- OTFoodAndBeverages.php
+++ OTFoodAndBeverages.php
@@ -14,12 +14,6 @@
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	</head>
 	<style>
-		.containers
-		{
-			margin: 10px;
-			
-		}
-		
 		.each_cont
 		{
 			margin-top: 20px;
@@ -36,7 +30,7 @@
 		include 'include/OTNavBar.php';
 		?>
 	<div class="container-fluid">
-		<div class="row containers">
+		<div class="row col-lg-12">
 			<?php
 				include "backend/DatabaseConnect.php";
 
