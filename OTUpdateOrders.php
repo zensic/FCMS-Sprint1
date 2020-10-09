4fcfc5deada19c84b60cd6c74d9f4f4b25ecc61d
simonlu97 <62094893+simonlu97@users.noreply.github.com>
2020-10-09 19:48:05 +0800

    Update OTUpdateOrders.php

--- OTUpdateOrders.php
+++ OTUpdateOrders.php
@@ -8,21 +8,13 @@
 		<meta name = "viewport" content = "width=device-width, initial-scales">
 		
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<link rel="stylesheet" href="styles/OTUpdateOrder.css">
 		
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
 		
-		<style>
-			.table_heading{
-				background-color: #5E503F;
-				color: white;
-			}
-			.container{
-				margin-top: 50px;
-			}
-		</style>
 		<?php
 		include 'include/NavBarStyle.php';
 		?>
