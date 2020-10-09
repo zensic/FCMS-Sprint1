d1b5ae4af01aa8c6540aad6b1c58312b6ba85828
Alvin101210282 <101210282@students.swinburne.edu.my>
2020-10-09 14:47:30 +0800

    Implement BackEnd feature
    Insert a new operation team account after user clicked submit button

--- MT_OTAccCreation.php
+++ MT_OTAccCreation.php
@@ -56,33 +56,72 @@
 		include 'include/MTNavBar.php';
 		?>
 		<div class = "container-fluid">
+
 			<div class = "row">
 				<div class = "col-md-4 col-sm-4 col-xs-12"></div>
 				<div class = "col-md-4 col-sm-4 col-xs-12">
-					<form class="form-container">
+					<form class="form-container" method="post">
 					<h1>Operation Team</h1>
 					<h1>User Creation Page</h1>
 						<div class="form-group">
 							<label for="username">Username</label>
-							<input type="text" class="form-control" id="InputUsername">
+							<input type="text" class="form-control" id="InputUsername" name="OT_username" placeholder="Alphabetical characters only" pattern="[A-Za-z]">
 						 </div>
 						<div class="form-group">
 							<label for="InputEmail1">Facebook Link</label>
-							<input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp">
+							<input type="text" class="form-control" id="InputEmail1" aria-describedby="emailHelp" pattern = "(?:(?:http|https):\/\/)?(?:www.)?facebook.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[?\w\-]*\/)?(?:profile.php\?id=(?=\d.*))?([\w\-]*)?" placeholder="Profile page" name="OT_FacebookLink">
 						</div>
 						<div class="form-group">
 							<label for="InputPassword1">New Password</label>
-							<input type="password" class="form-control" id="InputPassword1">
+							<input type="password" class="form-control" id="InputPassword1" name="OT_password" pattern="[0-9A-za-z]{8,12}" placeholder="8-12 Characters, Aplhanumeric Only">
 						</div>
 						<div class="form-group">
 							<label for="InputPassword1">Confirm Password</label>
-							<input type="password" class="form-control" id="InputPassword1">
+							<input type="password" class="form-control" id="InputPassword1" placeholder="Retype your password">
 						</div>
-						<button type="submit" class="btn btn-warning btn-block">Submit</button>
+						<button type="submit" class="btn btn-warning btn-block" name="submit">Submit</button>
 					</form>
 				</div>
 				<div class = "col-md-4 col-sm-4 col-xs-12"></div>
 			</div>
+
 		</div>
+
+		<!--Insert a new operation team account-->
+		<?php	
+			// Create database connection
+			$db = mysqli_connect("localhost", "root", "", "fcms");
+				
+			//Uncomment this section to check database connection
+			/*if($db){
+				echo"Successful Connect to DB<br/>";
+			}else{
+				die("fail");
+			}*/
+
+			//Sanitise the input
+			function sanitise_input($data) {
+				$data = trim($data);
+				$data = stripslashes($data);
+				$data = htmlspecialchars($data);
+				return $data;
+			}
+
+			//Make sure user clicked the submit button
+			if(isset($_POST['submit'])){
+				$username = mysqli_real_escape_string($db, sanitise_input($_POST['OT_username']));
+				$FB_Link = mysqli_real_escape_string($db, sanitise_input($_POST['OT_FacebookLink']));
+				$password = mysqli_real_escape_string($db, sanitise_input($_POST['OT_password']));
+
+				$sql = "INSERT INTO employee (username, password, facebookLink) VALUES ('$username','$password','$FB_Link')";
+				// execute query
+				/*if (mysqli_query($db, $sql)){
+					echo"Successfully insert<br/>";	
+				}else{
+					echo"Failed to insert</br>";
+				}*/
+			}
+			$db->close();
+		?>
 	</body>
 </html>
