b47d5a0fb19ce4ccc112f1c8ef388beef0366e1b
jinny990 <junyee9@gmail.com>
2020-10-09 10:56:51 +0800

    Client footer css& php includes

---
+++ include/ClientFooter.php
@@ -0,0 +1,24 @@
+<?php
+    echo'
+
+    <footer class="bg-primary text-white" id="footermain">
+    <div id="footerimage">
+      <div class="container py-12">
+        <div class="row text-center">
+          <div class="col-lg-12 ">
+            <h4>About Us</h4>
+            <p> 082-8865234</p>
+            <p>enquiry@foodedge.com</p>
+            <p>Jalan Song</p>
+            <p>Kuching, Sarawak, Malaysia</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid text-center bg-secondary" id="copyright">
+      &copy; 2020. All right are Reserved by FoodEdge Gourmate
+    </div>
+
+  </footer>
+'
+?>
