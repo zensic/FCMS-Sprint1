34eee5889dffbb2e62f01da4104d571bfddbbef3
simonlu97 <62094893+simonlu97@users.noreply.github.com>
2020-10-09 19:48:00 +0800

    Update MT_OTAccCreation.php

--- MT_OTAccCreation.php
+++ MT_OTAccCreation.php
@@ -8,6 +8,7 @@
 		<meta name = "viewport" content = "width=device-width, initial-scales">
 		
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<link rel="stylesheet" href="styles/MT_OTAccCreation.css">
 		
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
@@ -15,49 +16,15 @@
 		<?php
         include 'include/NavBarStyle.php';
   		?>
-
-		<style>
-			*
-			{
-				color: #5E503F;
-				font-weight: bold;
-			}
-			
-			h1
-			{
-				text-align: center;
-			}
-
-			body
-			{
-				background-image: url("images/background.jpg");
-				background-repeat: no-repeat;
-				background-position: center;
-				background-size: cover;
-				background-attachment: fixed;
-				
-			}
-			.form-container
-			{
-				border: 0px solid #fff; 
-				padding: 30px 30px; 
-				margin-top: 10vh;
-				box-shadow: -1px 4px 26px 11px rgba(0,0,0,0.50);
-				background-color:white;
-			}
-
-			.table-sortable tbody tr {
-				cursor: move;
-			}
-		</style>
+		
 	</head>
 	<body>
 		<?php
 		include 'include/MTNavBar.php';
 		?>
-		<div class = "container-fluid">
+		<div class = "container-fluid ">
 
-			<div class = "row">
+			<div class = "row box_1">
 				<div class = "col-md-4 col-sm-4 col-xs-12"></div>
 				<div class = "col-md-4 col-sm-4 col-xs-12">
 					<form class="form-container" method="post">
