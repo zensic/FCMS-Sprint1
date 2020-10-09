a486ef28e67eb53ce1141abe4180d3bb8dd4ad4a
Alvin101210282 <62094486+Alvin101210282@users.noreply.github.com>
2020-10-09 20:49:20 +0800

    Add files via upload
    Implement Back End Feature, insert new row into client table when client clicked the sign up button.

--- ClientAccCreation.php
+++ ClientAccCreation.php
@@ -31,7 +31,7 @@
     ?>
     
     <div class="signup-form">
-        <form action="#" method="post">
+        <form method="post">
             <div class="card text-white" style="width: 330px">
                 <div class="card-body">
                     <h2>Create Client Account</h2>
@@ -113,19 +113,19 @@
                     <span class="input-group-addon">
                         <i class="fa fa-building"></i>
                     </span>
-                    <select id="inputState" class="form-control">
-                        <option selected>Choose a state</option>
-                        <option>Kuala Lumpur</option>
-                        <option>Petaling Jaya</option>
-                        <option>Kuala Terengganu</option>
-                        <option>Seremban</option>
-                        <option>Subang Jaya</option>
-                        <option>Shah alam</option>
-                        <option>George Town</option>
-                        <option>Alor Setar</option>
-                        <option>Ipoh</option>
-                        <option>Melaka</option>
-                        <option>Johor Bahru</option>
+                    <select id="inputState" name="state" class="form-control">
+                        <option selected value="">Choose a state</option>
+                        <option value="Kuala Lumpur">Kuala Lumpur</option>
+                        <option value="Petaling Jaya">Petaling Jaya</option>
+                        <option value="Kuala Terengganu">Kuala Terengganu</option>
+                        <option value="Seremban">Seremban</option>
+                        <option value="Subang Jaya">Subang Jaya</option>
+                        <option value="Shah alam">Shah alam</option>
+                        <option value="George Town">George Town</option>
+                        <option value="Alor Setar">Alor Setar</option>
+                        <option value="Ipoh">Ipoh</option>
+                        <option value="Melaka">Melaka</option>
+                        <option value="Johor Bahru">Johor Bahru</option>
                     </select>
                 </div>
             </div>
@@ -135,19 +135,19 @@
                     <span class="input-group-addon">
                         <i class="fa fa-building"></i>
                     </span>
-                    <input type="number" class="form-control" name="zip_code" placeholder="Zip Code" required="required">
+                    <input type="text" class="form-control" name="zip_code" placeholder="Zip Code" required="required">
                 </div>
             </div>
           </div>
         </div>
 
             <div class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input" id="membershipCheckBox">
+                <input type="checkbox" class="custom-control-input" id="membershipCheckBox" name="createMembership">
                 <label class="custom-control-label" for="membershipCheckBox">Apply a membership account for RM 10.00</label>
             </div>
 
             <div class="form-group">
-                <button type="submit" class="btn btn-primary btn-block btn-lg">Sign Up</button>
+                <button type="submit" name="signUp" class="btn btn-primary btn-block btn-lg">Sign Up</button>
             </div>
 
             <div class="text-center">Already have an account? <a href="#">Login here</a>.</div>
@@ -155,5 +155,58 @@
             <p class="small text-center">By clicking the Sign Up button, you agree to our <br><a href="#">Terms &amp; Conditions</a>, and <a href="#">Privacy Policy</a>.</p>
         </form>
     </div>
+
+    <!--Insert a new client account-->
+	<!--?php	
+		// Create database connection
+		$db = mysqli_connect("localhost", "root", "", "fcms");
+				
+		//Uncomment this section to check database connection
+		if($db){
+			echo"Successful Connect to DB<br/>";
+		}else{
+			die("fail");
+		}
+
+		//Sanitise the input
+		function sanitise_input($data) {
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);
+			return $data;
+		}
+
+		//Make sure user clicked the submit button
+		if(isset($_POST['signUp'])){
+            $memberID = rand(1,200);
+            if(isset($_POST['createMembership'])){
+                $point = 0;
+                $sql1 = "INSERT INTO member (member_ID,member_point) VALUES ('$memberID','$point')";
+                // execute query
+			    if (mysqli_query($db, $sql1)){
+				    echo"<br/>Successfully insert member<br/>";	
+			    }else{
+				    echo"<br/>Failed to insert member</br>";
+			    }
+            }
+
+            //Escape special characters for $db connection
+            $clientID = rand(1,200);
+			$username = mysqli_real_escape_string($db, sanitise_input($_POST['username']));
+            $email = mysqli_real_escape_string($db, sanitise_input($_POST['email']));
+            $password = mysqli_real_escape_string($db, sanitise_input($_POST['password']));
+            $phoneNum = mysqli_real_escape_string($db, sanitise_input($_POST['phone_number']));
+            $address = mysqli_real_escape_string($db, sanitise_input($_POST['floor_unit'].$_POST['street_address'].$_POST['city'].$_POST['state'].$_POST['zip_code']));
+
+            $sql2 = "INSERT INTO client (client_ID, member_ID, username, email, password, phone_number, address) VALUES ('$clientID', 001,'$username', '$email','$password', '$phoneNum', '$address')";
+			// execute query
+			if (mysqli_query($db, $sql2)){
+				echo"<br/>Successfully insert<br/>";	
+			}else{
+				echo"<br/>Failed to insert</br>";
+			}
+		}
+		$db->close();
+	?-->
 </body>
 </html>
