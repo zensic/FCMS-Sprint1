8ccd6f3888ea34ea3474349ed6b4d46cbd04d565
zensic <anthonyleehj@gmail.com>
2020-10-09 12:36:26 +0800

    Updated links for navbar

--- include/ClientsNavBar.php
+++ include/ClientsNavBar.php
@@ -1,15 +1,5 @@
 <?php
     echo'
-    <head>
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-  
-    <!-- Custom styles for this template -->
-    <link href="styles/client_catering.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
-  
-  </head>
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
     <div class="container">
--- include/NavBarStyle.php
+++ include/NavBarStyle.php
@@ -5,8 +5,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   
     <!-- Custom styles for this template -->
-    <link href="styles/client_catering.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
+    <link href="styles/navbar.css" rel="stylesheet">
 
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
