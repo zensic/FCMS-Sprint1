014ca9f9ef1dfe618aea1c46fccdda93decad433
zensic <anthonyleehj@gmail.com>
2020-10-22 21:40:11 +0800

    Edited OTCateringPackage1.php

--- OTCateringPackage1.php
+++ OTCateringPackage1.php
@@ -23,45 +23,40 @@
   <div class="container" >
     <h1 class="text-center">Catering Menu</h1>
     <hr class="hr">
-    </div>
-
-    <div class="container" >
-      <div class="row">
-        <div class="col-md-4 col-sm-6">
-          <div class="product-grid2">
-            <div class="product-image2"> <a href="#"> <img class="pic-1" src=<?php echo $image_packageA ?>> </a>
-            </div>
-            <div class="product-content">
-              <h3 class="title"><a href="#"><?php echo 'Chinese Cuisine' ?></a></h3>
-              <button type="button" class="btn btn-secondary badge-pill " id="btn_edit"> Edit </button>
-            </div>
+  </div>
+  <div class="container" >
+    <div class="row">
+      <div class="col-md-4 col-sm-6">
+        <div class="product-grid2">
+          <div class="product-image2">
+            <a href="OTCateringPackage2.php"> <img class="pic-1" src=<?php echo $image_packageA ?>> </a>
+          </div>
+          <div class="product-content">
+            <button type="button" class="btn btn-secondary badge-pill " id="btn_edit"><a href="OTCateringPackage2.php">Edit <?php echo $name_packageA ?></a></button>
           </div>
         </div>
-        <div class="col-md-4 col-sm-6">
-          <div class="product-grid2 ">
-            <div class="product-image2"> <a href="#"> <img class="pic-1" src=<?php echo $image_packageB ?>> </a>
-
-            </div>
-            <div class="product-content">
-              <h3 class="title"><a href="#">Indian Cursine</a></h3>
-              <button type="button" class="btn btn-secondary badge-pill " id="btn_edit"> Edit </button>
-            </div>
+      </div>
+      <div class="col-md-4 col-sm-6">
+        <div class="product-grid2 ">
+          <div class="product-image2"> 
+            <a href="OTCateringPackage2.php"> <img class="pic-1" src=<?php echo $image_packageB ?>> </a>
+          </div>
+          <div class="product-content">
+            <button type="button" class="btn btn-secondary badge-pill " id="btn_edit"><a href="OTCateringPackage2.php">Edit <?php echo $name_packageB ?></a></button>
           </div>
         </div>
-        <div class="col-md-4 col-sm-6">
-          <div class="product-grid2">
-            <div class="product-image2"> <a href="#"> <img class="pic-1" src=<?php echo $image_packageC ?>> </a>
-            </div>
-            <div class="product-content">
-              <h3 class="title"><a href="#">Mix Cursine</a></h3>
-              <button type="button" class="btn btn-secondary badge-pill " id="btn_edit"> Edit </button>
-            </div>
+      </div>
+      <div class="col-md-4 col-sm-6">
+        <div class="product-grid2">
+          <div class="product-image2">
+            <a href="OTCateringPackage2.php"> <img class="pic-1" src=<?php echo $image_packageC ?>> </a>
+          </div>
+          <div class="product-content">
+            <button type="button" class="btn btn-secondary badge-pill " id="btn_edit"><a href="OTCateringPackage2.php">Edit <?php echo $name_packageC ?></a></button>
           </div>
         </div>
-
       </div>
     </div>
- 
   </div>
 </body>
 
--- include/StringPath.php
+++ include/StringPath.php
@@ -1,9 +1,4 @@
 <?php
-// images path
-$image_packageA = "images\packages\packageA.jpg";
-$image_packageB = "images\packages\packageB.jpg";
-$image_packageC = "images\packages\packageC.jpg";
-
 // connect to the database
 include 'backend/DatabaseConnect.php'; // global variables for connection
 $db = new mysqli($SERVERNAME, $USERNAME, $PASSWORD, $DATABASE);
@@ -14,23 +9,24 @@ $result = $db->query($sql);
 $row = $result->fetch_assoc();
 $name_packageA = $row["PackageName"];
 $descr_packageA = $row["PackageDescription"];
-$price_packageA = $row["Price"];
+$price_packageA = $row["PricePerPax"];
+$image_packageA = $row["ImagePath"];
 
 // store data for packageB
 $row = $result->fetch_assoc();
 $name_packageB = $row["PackageName"];
 $descr_packageB = $row["PackageDescription"];
-$price_packageB = $row["Price"];
+$price_packageB = $row["PricePerPax"];
+$image_packageB = $row["ImagePath"];
 
 // store data for packageC
 $row = $result->fetch_assoc();
 $name_packageC = $row["PackageName"];
 $descr_packageC = $row["PackageDescription"];
-$price_packageC = $row["Price"];
+$price_packageC = $row["PricePerPax"];
+$image_packageC = $row["ImagePath"];
 
 // close db
 $db->close();
-
-// strings from database
 
 ?>
