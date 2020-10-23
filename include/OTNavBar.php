2cb35d39738b64c7fbabdfe10ddaf0c15852a2ce
bong98 <55526360+bong98@users.noreply.github.com>
2020-10-23 21:31:42 +0800

    Update OTNavBar.php
    Change NavBar

--- include/OTNavBar.php
+++ include/OTNavBar.php
@@ -1,23 +1,22 @@
 <?php
     echo '
     <!-- Navigation -->
-        <div class="container-fluid" id="OTmainNav">
-            <nav class="navbar navbar-expand-md navbar-light" >
-                <a class="navbar-brand" href="index.html"><img src="images/logo.png" width="50" id="logo"></a>
-                <a class="navbar-brand" href="index.html"><span id="company">FoodEdge</span></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="nav navbar-nav ml-auto smooth-scroll">
-                        <li class="nav-item"><a class="nav-link" href="OTCateringPackage1.php">Menu</a></li>
-                        <li class="nav-item"><a class="nav-link" href="OTUpdateOrders.php">Track</a></li>
-                        <li class="nav-item"><a class="nav-link" href="ClientHome.php">Logout</a></li>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <div class="container">
+        <a class="navbar-brand" href="#page-top"><img src="images/logo.png" width="50" id="logo"></a>
+        <a class="navbar-brand js-scroll-trigger" href="#page-top" id="company">FoodEdge</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a class="nav-link" href="OTCateringPackage1.php">Menu</a></li>
+                <li class="nav-item"><a class="nav-link" href="OTUpdateOrders.php">Track</a></li>
+                <li class="nav-item"><a class="nav-link" href="ClientHome.php">Logout</a></li></li>
+            </ul>
         </div>
+        </div>
+    </nav>
     '
     
 ?>
